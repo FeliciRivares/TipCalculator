32c3c3aa8d521ff8c94f4bba3478fca6f6f2bee4
Karina Ostrovska <karina.ostrovsika@gmail.com>
2023-02-07 00:43:04 +0200

    complate adaptiv lending

---
+++ main.js
@@ -0,0 +1,4 @@
+const totalPerPersonText = document.getElementById('billAmount');
+const totalNumberOfPersonText = document.getElementById('numberOfPerson');
+const calculateButton = document.getElementById('calculateButton');
+const 
